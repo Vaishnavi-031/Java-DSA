7c5f88f60324d65d6113099f87fab5ce72cd0c93
Vaishnavi-031 <machanivaishnavi@gmail.com>
2026-01-22 21:31:17 +0530

    Add find max element in array

---
+++ arrays/FindMax.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class pr {
+  public static int findMax(int[] arr){
+          int max = arr[0];
+          for(int i=0;i<arr.length;i++){
+              if(arr[i]>max){
+                  max=arr[i];
+              }
+          }
+          return max;
+      }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    for(int i=0;i<n;i++){
+      arr[i] = sc.nextInt();
+    }
+    System.out.println(findMax(arr));
+    }
+}
